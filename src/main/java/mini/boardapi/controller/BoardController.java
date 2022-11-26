996c08c3e744b9dc649fa1f45e1b9b07ba415f56
bookpark <bkbkgg@gmail.com>
2022-11-26 20:43:14 +0900

    주석 제거

--- src/main/java/mini/boardapi/controller/BoardController.java
+++ src/main/java/mini/boardapi/controller/BoardController.java
@@ -29,29 +29,29 @@ public class BoardController {
 
     private final BoardService boardService;
 
-//    @PostMapping("/api/write-board")
-//    public ResponseEntity<String> writeBoard(@RequestParam("writer") String writer,
-//                                             @RequestParam("password") String password,
-//                                             @RequestParam("subject") String subject,
-//                                             @RequestParam("content") String content,
-//                                             @RequestParam(name = "file", required = false) MultipartFile file) {
-//        ResponseEntity<String> res = null;
-//        try {
-//            String filename = null;
-//            if (file != null && !file.isEmpty()) {
-//                String path = "/Users/book/KFQ/java/";
-//                filename = file.getOriginalFilename();
-//                File dFile = new File(path + filename);
-//                file.transferTo(dFile);
-//            }
-//            boardService.writeBoard(new Board(null, writer, password, subject, content, filename));
-//            res = new ResponseEntity<String>("게시글 저장 성공", HttpStatus.OK);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            res = new ResponseEntity<String>("게시글 저장 실패", HttpStatus.BAD_REQUEST);
-//        }
-//        return res;
-//    }
+    @PostMapping("/api/write-board")
+    public ResponseEntity<String> writeBoard(@RequestParam("writer") String writer,
+                                             @RequestParam("password") String password,
+                                             @RequestParam("subject") String subject,
+                                             @RequestParam("content") String content,
+                                             @RequestParam(name = "file", required = false) MultipartFile file) {
+        ResponseEntity<String> res = null;
+        try {
+            String filename = null;
+            if (file != null && !file.isEmpty()) {
+                String path = "/Users/book/KFQ/java/";
+                filename = file.getOriginalFilename();
+                File dFile = new File(path + filename);
+                file.transferTo(dFile);
+            }
+            boardService.writeBoard(new Board(null, writer, password, subject, content, filename));
+            res = new ResponseEntity<String>("게시글 저장 성공", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            res = new ResponseEntity<String>("게시글 저장 실패", HttpStatus.BAD_REQUEST);
+        }
+        return res;
+    }
 
     @PostMapping("/api/write-board-v2")
     public ResponseEntity<String> writeBoardV2(@ModelAttribute Board board,
